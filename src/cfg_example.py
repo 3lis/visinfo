752d513a602f682bd3cdd51103210366ee402b83
Alice Plebe <alice.plebe@gmail.com>
2024-12-31 09:30:07 +0100

    Minor fix

--- src/cfg_example.py
+++ src/cfg_example.py
@@ -1,7 +1,7 @@
 kwargs      = {
     'model_id':         -1,
     'experiment':       "both",             # news_image, news_noimage, both
-    'dialogs':          [ "prologue_1", "ask_reliability" ],
+    'dialogs':          [ "prologue_1", "ask_true" ],
     'max_tokens':       100,
-    'n_returns':        10,
+    'n_returns':        4,
 }
