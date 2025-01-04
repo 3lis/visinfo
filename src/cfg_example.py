c0b6df7db35cf921e87b8ee06e443fe01afde389
Alice Plebe <alice.plebe@gmail.com>
2025-01-04 10:51:42 +0100

    Update cfg_example.py

--- src/cfg_example.py
+++ src/cfg_example.py
@@ -1,10 +1,10 @@
 kwargs      = {
     'model_id':         -1,                 # llava-next
     'experiment':       "both",             # news_image, news_noimage, both
-    'dialogs_pre':      [ "intro_profile", "profile_1", "intro_context" ],
-    'dialogs_post':     [ "ask_img", "ask_share"],
+    'dialogs_pre':      [ "intro_profile", "profile_moderate", "context_strict" ],
+    'dialogs_post':     [ "ask_img", "ask_share_strict"],
     # 'news_ids':         [ 'f001', 'f002' ],
-    'max_tokens':       300,
+    'max_tokens':       500,
     'n_returns':        5,
     'top_p':            1.0,
     'temperature':      0.3,
