e048fbd36fcab8c03ff9d1637d55dd4ebadf4184
Alice Plebe <alice.plebe@gmail.com>
2025-02-18 17:45:29 +0000

    New modules for plots and stats

---
+++ src/infstat.py
@@ -0,0 +1,527 @@
+"""
+#####################################################################################################################
+
+    Program for generating statistics over a range of executions in ../res
+
+#####################################################################################################################
+"""
+
+import  os
+import  sys
+import  re
+import  json
+import  time
+import  shutil
+import  numpy   as np
+import  pandas  as pd
+from    statsmodels.formula.api     import ols
+from    statsmodels.stats.anova     import anova_lm
+from    models                      import models_short_name
+import  plot
+
+# specification of the executions to analyze
+# if res_range is empty all executions found in ../res are analyzed
+# if res_range has only one entry, it is the first execution to process, followed by all the others
+# if res_range has two entries, these are the boundaries of the executions to analyze
+# if res_range has more than two entries, those entries only are analyzed
+res_range           = [ "25-02-05_11-48-20" ]   # this is the first execution with demographics
+res_range           = []
+
+res                 = "../res"                  # results directory
+dir_json            = "../data"                 # directory with all input data
+dir_stat            = "../stat"                 # output directory
+f_demo              = "demographics.json"       # filename of demographic data
+log                 = "log.txt"                 # filename of execution logs
+f_stat              = "stat.txt"                # filename of output statistics
+f_plot              = "pl"                      # filename prefix of output plots
+frmt_statdir        = "%y-%m-%d-%H-%M"          # datetime format for output directory
+columns             = (
+    "predia",          # the titles of preliminary dialogs
+    "postdia",         # the titles of final part of the dialogs
+    "profile",         # the profile induced to the model
+    "model",           # the model used
+    "gender",          # F/M
+    "race",            # race categories as in the demographics file
+    "edu",             # education categories as in the demographics file
+    "age",             # age categories as in the demographics file
+    "politic",         # political affiliation categories as in the demographics file
+    "news",            # the news code
+    "value",           # true/false
+    "yes_wimg",        # fraction of YES answer for text+image
+    "not_wimg",        # fraction of NO answer for text+image
+    "unk_wimg",        # fraction of missing answer for text+image
+    "yes_nimg",        # fraction of YES answer for text only
+    "not_nimg",        # fraction of NO answer for text only
+    "unk_nimg",        # fraction of missing answer for text only
+)
+
+shortcuts   = {
+    'ask_img':              'ask_im',
+    'ask_share':            'ask_sh',
+    'ask_share_strict':     'ask_ss',
+    'ask_share_noexplain':  'ask_ne',
+    'intro_profile':        'int_pr',
+    'profile_conspirator':  'pro_co',
+    'profile_moderate':     'pro_mo',
+    'profile_rational':     'pro_ra',
+    'context':              'context',
+    'context_strict':       'ctx_st',
+    'reason_3steps':        'rea_3s',
+    'reason_base':          'rea_bs',
+    'reason_share':         'rea_sh',
+    'reason_share_xml':     'rea_sx',
+    'reason_share_delimit': 'rea_sd',
+}
+
+demography          = None                      # dictionary with demographic categories
+
+def read_demo():
+    """
+    Read the demographic categories in the json file
+    """
+    global demography
+    dfile               = os.path.join( dir_json, f_demo )
+    with open( dfile, 'r' ) as f:
+        demography      = json.load( f )
+
+
+def get_demo( lines ):
+    """
+    Retrieve demographics info used in the statistics
+    """
+    gender          = "unspec"
+    race            = "unspec"
+    edu             = "unspec"
+    age             = "unspec"
+    politic         = "unspec"
+
+    for l in lines:
+        if "political_affiliation" in l:
+            politic     = l.lower().split()[ -1 ]
+        if "gender" in l:
+            if "male" in l:
+                gender  = 'M'
+            if "female" in l:
+                gender  = 'F'
+        if "race" in l:
+            for i, d in enumerate( demography[ "race" ] ):
+                if d in l:
+                    race    = f"type{i}"
+        if "education" in l:
+            for i, d in enumerate( demography[ "education" ] ):
+                if d in l:
+                    edu     = f"level{i}"
+        if "age" in l:
+            for i, d in enumerate( demography[ "age" ] ):
+                if d in l:
+                    age     = f"bin{i}"
+
+    return gender, race, edu, age, politic
+
+
+def get_predialog( line ):
+    """
+    Retrieve the pre-dialog settings
+    """
+    predia      = "unspec"
+    profile     = "unspec"
+    dialogs     = re.sub( r'[\W]+', ' ', line ).split()[ 1 : ]
+    if len( dialogs ) < 2:
+        print( f"not enough elements in {dialogs}" )
+        return predia, profile
+    if "profile_" in dialogs[ 1 ]:
+        profile = dialogs[ 1 ].replace( "profile_", "" )
+    if len( dialogs ) > 2:
+        if dialogs[ 2 ] in shortcuts.keys():
+            predia  = shortcuts[ dialogs[ 2 ] ]
+
+    return predia, profile
+
+
+def get_postdialog( line ):
+    """
+    Retrieve the post-dialog settings
+    """
+    postdia     = "unspec"
+    dialogs     = re.sub( r'[\W]+', ' ', line ).split()[ 1 : ]
+    if not len( dialogs ):
+        print( f"not enough elements in {line}" )
+        return postdia
+    if dialogs[ 0 ] in shortcuts.keys():
+        postdia = shortcuts[ dialogs[ 0 ] ]
+    if len( dialogs ) > 1:
+        if dialogs[ -1 ] in shortcuts.keys():
+            postdia  += shortcuts[ dialogs[ -1 ] ][ -3 : ]
+
+    return postdia
+
+
+def get_info( lines ):
+    """
+    Retrieve all info used in the statistics
+    """
+    predia      = "unspec"
+    postdia     = "unspec"
+    profile     = "unspec"
+    model       = "unspec"
+    gender      = "unspec"
+    race        = "unspec"
+    edu         = "unspec"
+    age         = "unspec"
+    politic     = "unspec"
+    news        = []
+    value       = []
+    yes_wimg    = []
+    not_wimg    = []
+    unk_wimg    = []
+    yes_nimg    = []
+    not_nimg    = []
+    unk_nimg    = []
+    for i, l in enumerate( lines ):
+        if "News" in l:
+            break
+        if "model " in l:
+            m_fullname      = l.split()[ -1 ]
+            model           = models_short_name[ m_fullname ]
+        if "dialogs_pre" in l:
+            predia, profile = get_predialog( l )
+        if "dialogs_post" in l:
+            postdia         = get_postdialog( l )
+        if "demographics" in l and not "None" in l:
+            demo_lines      = [ l ]
+            n   = 1
+            l   = lines[ i+n ]
+            while l[ : 2 ] == '  ':
+                demo_lines.append( l )
+                n   += 1
+                l   = lines[ i+n ]
+                if n > 6:
+                    print( "invalid demographic format" )
+                    return None
+                gender, race, edu, age, politic = get_demo( demo_lines )
+
+    n       = len( lines )
+    i       += 1
+    while True:
+        if "f_mn" in l:
+            break
+        if "t_mn" in l:
+            break
+        if "mean" in l:
+            break
+        l       = lines[ i ]
+        v       = l.split()
+        if len( v ) != 7:
+            print( f"invalid lenght of data: {v}" )
+            return None
+        news.append( v[ 0 ][ 1 : ] )
+        if v[ 0 ][ 0 ] == 'f':
+            value.append( 'false' )
+        else:
+            value.append( 'true' )
+        yes_wimg.append( float( v[ 1 ] ) )
+        not_wimg.append( float( v[ 2 ] ) )
+        unk_wimg.append( float( v[ 3 ] ) )
+        yes_nimg.append( float( v[ 4 ] ) )
+        not_nimg.append( float( v[ 5 ] ) )
+        unk_nimg.append( float( v[ 6 ] ) )
+        i       += 1
+        if i == n:
+            print( "missing end of news results" )
+            return None
+
+    news        = np.array( news )
+    value       = np.array( value )
+    yes_wimg    = np.array( yes_wimg )
+    not_wimg    = np.array( not_wimg )
+    unk_wimg    = np.array( unk_wimg )
+    yes_nimg    = np.array( yes_nimg )
+    not_nimg    = np.array( not_nimg )
+    unk_nimg    = np.array( unk_nimg )
+    predia      = np.full( news.shape, predia )
+    postdia     = np.full( news.shape, postdia )
+    profile     = np.full( news.shape, profile )
+    model       = np.full( news.shape, model )
+    gender      = np.full( news.shape, gender )
+    race        = np.full( news.shape, race )
+    edu         = np.full( news.shape, edu )
+    age         = np.full( news.shape, age )
+    politic     = np.full( news.shape, politic )
+
+    data            = {
+        "predia":          predia,
+        "postdia":         postdia,
+        "profile":         profile,
+        "model":           model,
+        "gender":          gender,
+        "race":            race,
+        "edu":             edu,
+        "age":             age,
+        "politic":         politic,
+        "news":            news,
+        "value":           value,
+        "yes_wimg":        yes_wimg,
+        "not_wimg":        not_wimg,
+        "unk_wimg":        unk_wimg,
+        "yes_nimg":        yes_nimg,
+        "not_nimg":        not_nimg,
+        "unk_nimg":        unk_nimg,
+    }
+
+    return data
+
+
+def collect_data():
+    """
+    Scan the results, collecting all data
+
+    return:             [pandas.core.frame.DataFrame] the data in pandas DataFrame
+    """
+
+    list_res    = sorted( os.listdir( res ) )
+    match len( res_range ):
+        case 1:
+            first   = res_range[ 0 ]
+            assert first in list_res, f"first specified result {first} not found"
+            i_first     = list_res.index( first )
+            list_res    = list_res[ i_first : ]
+        case 2:
+            first   = res_range[ 0 ]
+            last    = res_range[ -1 ]
+            assert first in list_res, f"first specified result {first} not found"
+            assert last in list_res, f"last specified result {last} not found"
+            i_first     = list_res.index( first )
+            i_last      = list_res.index( last )
+            list_res    = list_res[ i_first : i_last+1 ]
+        case _:
+            if len( res_range ):
+                list_res    = res_range
+
+    arrays  = dict()
+    for c in columns:
+        arrays[ c ] = []
+
+    for f in list_res:                          # scan all selected results
+        fname   = os.path.join( res, f, log )
+        if not os.path.isfile( fname ):
+            print( f"{f}  is not a file" )
+            continue
+        with open( fname, 'r' ) as fd:
+            lines   = fd.readlines()
+        if not len( lines ):
+            print( f"{f}  has no lines" )
+            continue
+        data        = get_info ( lines )        # get data for one execution
+        if data is None:
+            print( f"{f}  no info found" )
+            continue
+        for c in columns:                       # accumulate data
+            arrays[ c ].append( data[ c ] )
+        print( f"{f}  done" )
+
+    for c in columns:
+        v           = arrays[ c ]
+        arrays[ c ] = np.concatenate( v )
+
+    df          = pd.DataFrame( arrays )
+    return df
+
+
+def means( df ):
+    """
+    Compute means of the main scores grouped by the main variables independently.
+    Use the basic grouping and aggregation functions of pandas, and aggregate by models
+    and all the other variables.
+
+    params:
+        df              [pandas.core.frame.DataFrame] the data in pandas DataFrame
+
+    return:             [tuple] means for profile, age, gender, race, edu, politic (all models), and same by models
+    """
+    scores      = [ 'yes_wimg', 'yes_nimg' ]
+
+    mm          = df.groupby( [ 'value', 'profile' ] )[ scores ].mean()
+    ma          = df.groupby( [ 'value', 'age' ] )[ scores ].mean()
+    mg          = df.groupby( [ 'value', 'gender' ] )[ scores ].mean()
+    mr          = df.groupby( [ 'value', 'race' ] )[ scores ].mean()
+    me          = df.groupby( [ 'value', 'edu' ] )[ scores ].mean()
+    mp          = df.groupby( [ 'value', 'politic' ] )[ scores ].mean()
+    mmm         = df.groupby( [ 'model', 'value', 'profile' ] )[ scores ].mean()
+    mma         = df.groupby( [ 'model', 'value', 'age' ] )[ scores ].mean()
+    mmg         = df.groupby( [ 'model', 'value', 'gender' ] )[ scores ].mean()
+    mmr         = df.groupby( [ 'model', 'value', 'race' ] )[ scores ].mean()
+    mme         = df.groupby( [ 'model', 'value', 'edu' ] )[ scores ].mean()
+    mmp         = df.groupby( [ 'model', 'value', 'politic' ] )[ scores ].mean()
+
+    return mm, ma, mg, mr, me, mp, mmm, mma, mmg, mmr, mme, mmp
+
+
+def anova_1( df, score ):
+    """
+    Compute one-way anova of one the scores for all variables.
+    Construct the regression formula in the 'R'-style used in the ols function of statsmodels,
+    then apply the anova_lm to the regression model returned by ols
+    See:
+        https://www.statsmodels.org/dev/generated/statsmodels.formula.api.ols.html
+        https://www.statsmodels.org/dev/generated/statsmodels.stats.anova.anova_lm.html
+
+    params:
+        df              [pandas.core.frame.DataFrame] the data in pandas DataFrame
+        score           [str] one of the response scores
+
+    return:             [tuple] anova for profile, age, gender, race, edu, politic
+    """
+    formula     = f"{score} ~ C(profile)"
+    model       = ols( formula, df ).fit()
+    am          = anova_lm( model )
+    formula     = f"{score} ~ C(age)"
+    model       = ols( formula, df ).fit()
+    aa          = anova_lm( model )
+    formula     = f"{score} ~ C(gender)"
+    model       = ols( formula, df ).fit()
+    ag          = anova_lm( model )
+    formula     = f"{score} ~ C(race)"
+    model       = ols( formula, df ).fit()
+    ar          = anova_lm( model )
+    formula     = f"{score} ~ C(edu)"
+    model       = ols( formula, df ).fit()
+    ae          = anova_lm( model )
+    formula     = f"{score} ~ C(politic)"
+    model       = ols( formula, df ).fit()
+    ap          = anova_lm( model )
+
+    return am, aa, ag, ar, ae, ap
+
+
+def do_plots( df ):
+    """
+    Do various plots
+    """
+    fname       = os.path.join( dir_stat, f_plot )
+    plot.plot_models( df, groups=[ "value", "age" ], fname=fname )
+    plot.plot_models( df, groups=[ "value", "gender" ], fname=fname )
+    plot.plot_models( df, groups=[ "value", "race" ], fname=fname )
+    plot.plot_models( df, groups=[ "value", "edu" ], fname=fname )
+    plot.plot_models( df, groups=[ "value", "politic" ], fname=fname )
+    plot.plot_models( df, groups=[ "value", "profile" ], fname=fname )
+    plot.plot_models( df, groups=[ "value", "predia" ], fname=fname )
+    plot.plot_models( df, groups=[ "value", "postdia" ], fname=fname )
+    unk = [ "unk_wimg", "unk_nimg" ]
+    plot.plot_models( df, groups=[ "value", "predia" ], values=unk, fname=fname+"_unk" )
+    plot.plot_models( df, groups=[ "value", "postdia" ], values=unk, fname=fname+"_unk" )
+
+
+def print_means( f, m ):
+    """
+    Print mean values
+    """
+    mm, ma, mg, mr, me, mp = m
+    f.write( 80 * "=" + "\n" )
+    f.write( " means ".center( 80, ' ' ) + '\n' )
+    f.write( 80 * "=" + "\n\n" )
+    f.write( " profile ".center( 80, '=' ) + '\n' )
+    f.write( mm.to_string() + '\n\n' )
+    f.write( " age ".center( 80, '=' ) + '\n' )
+    f.write( ma.to_string() + '\n\n' )
+    f.write( " gender ".center( 80, '=' ) + '\n\n' )
+    f.write( mg.to_string() + '\n\n' )
+    f.write( " race ".center( 80, '=' ) + '\n' )
+    f.write( mr.to_string() + '\n\n' )
+    f.write( " education ".center( 80, '=' ) + '\n\n' )
+    f.write( me.to_string() + '\n\n' )
+    f.write( " politic ".center( 80, '=' ) + '\n\n' )
+    f.write( mp.to_string() + '\n\n' )
+    f.write( 80 * "=" + "\n\n" )
+
+
+def print_anova_1( f, a, scores ):
+    """
+    Print one-way anova
+    """
+    f.write( 80 * "=" + "\n" )
+    f.write( " one-way anova ".center( 80, ' ' ) + '\n' )
+    f.write( 80 * "=" + "\n\n" )
+    f.write( " profile ".center( 80, '=' ) + '\n' )
+    for s in scores:
+        f.write( s.center( 80, '-' ) + '\n' )
+        f.write( a[ s ][ 0 ].to_string() + '\n\n' )
+    f.write( " age ".center( 80, '=' ) + '\n' )
+    for s in scores:
+        f.write( s.center( 80, '-' ) + '\n' )
+        f.write( a[ s ][ 1 ].to_string() + '\n\n' )
+    f.write( " gender ".center( 80, '=' ) + '\n' )
+    for s in scores:
+        f.write( s.center( 80, '-' ) + '\n' )
+        f.write( a[ s ][ 2 ].to_string() + '\n\n' )
+    f.write( " race ".center( 80, '=' ) + '\n' )
+    for s in scores:
+        f.write( s.center( 80, '-' ) + '\n' )
+        f.write( a[ s ][ 3 ].to_string() + '\n\n' )
+    f.write( " education ".center( 80, '=' ) + '\n' )
+    for s in scores:
+        f.write( s.center( 80, '-' ) + '\n' )
+        f.write( a[ s ][ 4 ].to_string() + '\n\n' )
+    f.write( " politic ".center( 80, '=' ) + '\n' )
+    for s in scores:
+        f.write( s.center( 80, '-' ) + '\n' )
+        f.write( a[ s ][ 5 ].to_string() + '\n\n' )
+    f.write( 80 * "=" + "\n\n" )
+
+
+def do_stat( df ):
+    """
+    Do basic statistics and write it on file
+    """
+    scores      = [ 'yes_wimg', 'yes_nimg' ]
+    models      = list( df[ "model" ].unique() )
+    fname       = os.path.join( dir_stat, f_stat )
+    f           = open( fname, 'w' )
+    f.write( 80 * "=" + "\n\n" )
+    all_means   = means( df )
+    f.write( 80 * "+" + "\n" )
+    f.write( "all models".center( 80, ' ' ) + '\n' )
+    f.write( 80 * "+" + "\n\n" )
+    print_means( f, all_means[ : 6 ] )
+    f.write( "\n\n" )
+    f.write( 80 * "+" + "\n" )
+    f.write( "by model".center( 80, ' ' ) + '\n' )
+    f.write( 80 * "+" + "\n\n" )
+    print_means( f, all_means[ 6 : ] )
+    anova                   = {}
+    f.write( "\n\n" )
+    f.write( 80 * "+" + "\n" )
+    f.write( "all models".center( 80, ' ' ) + '\n' )
+    f.write( 80 * "+" + "\n\n" )
+    for s in scores:
+        anova[ s ]          = anova_1( df, s )
+    print_anova_1 ( f, anova, scores )
+    for m in models:
+        f.write( "\n\n" )
+        f.write( 80 * "+" + "\n" )
+        f.write( m.center( 80, ' ' ) + '\n' )
+        f.write( 80 * "+" + "\n\n" )
+        for s in scores:
+            anova[ s ]          = anova_1( df[ (df[ 'model' ]==m ) ], s )
+        print_anova_1 ( f, anova, scores )
+    f.close()
+
+
+
+# ===================================================================================================================
+#
+#   MAIN
+#
+# ===================================================================================================================
+if __name__ == '__main__':
+    # first, it is necessary to read the demographics from json
+    read_demo()
+    df          = collect_data()        # all data in pandas DataFrame
+    now_time    = time.strftime( frmt_statdir )
+    dir_stat    = os.path.join( dir_stat, now_time )
+    if not os.path.isdir( dir_stat ):
+        os.makedirs( dir_stat )
+    # save a copy of this file and the plotting script
+    shutil.copy( "infstat.py", dir_stat )
+    shutil.copy( "plot.py", dir_stat )
+    do_plots( df )
+    do_stat( df )
---
+++ src/plot.py
@@ -0,0 +1,156 @@
+"""
+#####################################################################################################################
+
+    Module for plotting
+
+#####################################################################################################################
+"""
+
+import  os
+import  sys
+import  copy
+import  pickle
+import  numpy               as np
+import  pandas              as pd
+from    matplotlib          import pyplot
+from    matplotlib.patches  import Patch, Polygon
+from    matplotlib.lines    import Line2D
+
+figsize         = ( 18.0, 8.0 )                             # figure size in inches
+labelspacing    = 1.2
+extension       = ".pdf"
+
+# generic set of colors, grouped 4x4
+# therefore in case of single group, better to use every 4 color,
+# and in case of group of 2, every 2 color
+dim_colors  = (
+        '#e88030',
+        '#ea3030',
+        '#eac130',
+        '#ed6ecb',
+        '#129dad',
+        '#124dfd',
+        '#1740ed',
+        '#19bd9d',
+        '#70a3ff',
+        '#5d8500',
+        '#62cf02',
+        '#00c28b',
+        '#00f25b',
+        '#00b29b',
+        '#00a2bb',
+        '#af907f',
+        '#afb01f',
+        '#af509f',
+        '#a0907f',
+)
+# markers
+dim_markers = ( 'o',  '*',  'D',  'P',  'X',  'h', 'H', '<', '>', 'x' )
+
+char_len    = 0.009                                     # typical length of a character in legend, in plot units
+
+
+
+def plot_values( df, groups=["value","age"], values=["yes_wimg", "yes_nimg"], fname="plot", suptitle='' ):
+    """
+    Generate one plot of the specified values, for a specified group of independent variables
+
+    params:
+        df          [pandas.core.frame.DataFrame] the data in pandas DataFrame
+        groups      [list] columns of independent variables
+        values      [list] the numerical values to be plot
+        fname       [str] name of the output file
+        suptitle    [str] plot title
+    """
+
+    columns = df.columns
+    y       = []                        # list of all vectors to plot
+    labels  = []                        # labels for all combinations of independent variables
+    cat     = dict()                    # all categorical entries found for independent variables
+    comb    = dict()                    # vectors for all combinations of independent variables
+    for g in groups:                    # gather the categorical entries for independent variables
+        assert g in columns, f"there is no column named {g}"
+        cat[ g ]    = list( df[ g ].unique() )
+
+    for g in groups:                    # collect all vectors for combinations of independent variables
+        if not len( comb ):
+            first           = True
+        else:
+            first           = False
+            new_comb        = dict()
+        for c in cat[ g ]:
+            if first:
+                comb[ c ]   = df[ df[ g ] == c ]
+            else:
+                for prev in comb.keys():
+                    dframe  = comb[ prev ]
+                    new     = prev + '-' + c
+                    new_comb[ new ] = dframe[ dframe[ g ] == c ]
+        if not first:
+            comb            = new_comb
+
+    for c in comb.keys():               # assign vectors to y and their key combinations to labels
+        dframe  = comb[ c ]
+        for v in values:
+            y.append( dframe [ v ] )
+            labels.append( f"{v} for {c}" )
+
+    n           = len( y )
+    ng          = len( df[ groups[ 0 ] ].unique() ) * len( values )
+    # compute a proper separation of bars by group
+    x           = [  i + i // ng for i in range( n ) ]
+    step        = 1. / ( x[ -1 ] + 1 )
+    match ng:                           # see comments in the definition of dim_colors
+        case 1:
+            colors      = dim_colors[ 0:4*n:4 ]
+        case 2:
+            colors      = dim_colors[ 0:2*n:2 ]
+        case _:
+            colors      = dim_colors[ : n ]
+
+    pyplot.rcParams.update( { "font.size": 14 } )
+    handles     = [ Patch( facecolor=c, label=l ) for c,l in zip( colors, labels ) ]
+    fig, ax     = pyplot.subplots( figsize=figsize )
+
+    for i, ( xx, data ) in enumerate( zip( x, y ) ):
+        m   = data.mean()
+        s   = data.std() / 2.
+        ax.bar( xx, m, yerr=s, color=colors[ i ], linewidth=2 )
+        ax.set_xticks( [] )
+
+    ax.set_ylabel( "YES fraction" )
+    ax.set_ylim( bottom=0.0, top=1.0 )
+
+    # compute an estimate of the max occupancy of labels in the plot
+    lab_len     = char_len * max( [ len( l ) for l in labels ] )
+    # compute the current geometry of the plotting box
+    box = ax.get_position()
+    # and now make room on the right for the labels
+    ax.set_position([box.x0, box.y0, box.width * ( 1 - lab_len ), box.height])
+    ax.legend( handles=handles, loc="center", bbox_to_anchor=(1+lab_len, 0.5), labelspacing=labelspacing )
+    fname       = fname + extension
+    pyplot.suptitle( suptitle, x=0.4 )
+    pyplot.savefig( fname, bbox_inches="tight", pad_inches=0 )
+    pyplot.close()
+    pyplot.clf()
+
+
+def plot_models( df, groups=["value","age"], values=["yes_wimg", "yes_nimg"], fname="pl" ):
+    """
+    Wrapper for executing plot_values() on all models found in the dataset, together with a final
+    plot with all models data
+
+    params:
+        df          [pandas.core.frame.DataFrame] the data in pandas DataFrame
+        groups      [list] columns of independent variables
+        values      [list] the numerical values to be plot
+        fname       [str] name of the output file
+    """
+    models      = list( df[ "model" ].unique() )    # find all models used in the dataframe
+
+    g           = groups[ -1 ]
+    for m in models:
+        name    = f"{fname}_{m}_{g}"
+        plot_values( df[ df[ "model" ] == m ], groups=groups, values=values, fname=name, suptitle=f"model {m}" )
+    name    = f"{fname}_all_{g}"
+    plot_values( df, groups=groups, values=values, fname=name, suptitle="all models" )
